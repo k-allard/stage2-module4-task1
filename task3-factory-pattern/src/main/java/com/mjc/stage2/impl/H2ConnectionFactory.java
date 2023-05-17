27c79ef66cb033e9686dcda86c098b21af77441e
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-17 15:18:38 +0400

    Done 3/3 tasks

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,40 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("h2database.properties"))){
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Class.forName(props.getProperty("jdbc_driver"));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        String url = props.getProperty("db_url");
+        String username = props.getProperty("user");
+        String password = props.getProperty("password");
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
 
