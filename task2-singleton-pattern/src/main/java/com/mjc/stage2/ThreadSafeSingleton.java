8afbf937a2c8ede80f327f541279a5f28ff22fb5
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-17 17:30:13 +0400

    Change singleton implementation

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,12 +1,15 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static volatile ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static volatile ThreadSafeSingleton instance;
 
     private ThreadSafeSingleton() {
     }
 
     public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
         return instance;
     }
 }
