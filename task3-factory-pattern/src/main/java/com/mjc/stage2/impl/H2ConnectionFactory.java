4bf306eb31cd7a9289ba8aa4c5c29c6ae439cb86
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-05-17 16:55:11 +0400

    Remove unused imports

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -4,10 +4,7 @@ import com.mjc.stage2.ConnectionFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
